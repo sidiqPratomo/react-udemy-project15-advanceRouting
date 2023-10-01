f6f0d0531cffec48a7c8d6f431820c423f69235b
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-10-01 14:59:08 +0700

    add defer to speed up page load

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { json, useLoaderData } from "react-router-dom";
+import { defer, json, useLoaderData, Await } from "react-router-dom";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
   // if (data.isError) {
   //   return <p>{data.message}</p>;
   // }
-  const events = data.events;
+  // const events = data.events;
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>loading...</p>}>
+      <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -31,6 +31,14 @@ export async function loader() {
       },
     );
   } else {
-    return response;
+    // return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
